f7829feca11bf2622db8dd5c1ac8914e79f1fbd5
yyammi <yamilaperez1394@gmail.com>
2026-05-13 14:21:02 -0300

    merge issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import ReportForm from "./components/ReportForm";
+
 import Navbar from "./Componentes/Navbar/Navbar";
 import Hero from "./Componentes/Hero/Hero";
 import Footer from "./Componentes/Footer/Footer";
@@ -8,6 +10,7 @@ function App() {
     <div className="landing-container">
       <Navbar />
       <h1 className="main-title">Red Ciudadana de Alerta Temprana de Pumas</h1>
+      <ReportForm />
       <Hero />
       <Footer />
     </div>
