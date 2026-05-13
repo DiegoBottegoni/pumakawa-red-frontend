a38fefcdeea00f8c0c43d7dd527be4cc2d8f25a4
lolamaffioli <lolapmaffioli@gmail.com>
2026-05-13 13:38:17 -0300

    logos agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import ReportForm from "./ReportForm";
+import ReportForm from "./components/ReportForm";
 
 function App() {
+
   return (
     <>
+      <h1>Pumakawa Red</h1>
       <ReportForm />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
